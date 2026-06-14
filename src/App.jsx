696577ac6a3cf5aa76cb2715708ec63129457ae3
ivengan <ivengan@users.noreply.github.com>
2026-06-14 23:21:42 +0800

    uodate browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PinLock from './pages/PinLock';
 import Dashboard from './pages/Dashboard';
 import Calendar from './pages/Calendar';
